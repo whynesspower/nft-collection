c24ada5ac0b0abcccf39d1ff966de798df96e8fd
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-05 19:11:48 +0530

    nft collection smart contract finished

--- hardhat/contracts/whynesspower.sol
+++ hardhat/contracts/whynesspower.sol
@@ -40,7 +40,7 @@ contract Whynesspower is ERC721Enumerable, Ownable {
 
     /**
       * @dev ERC721 constructor takes in a `name` and a `symbol` to the token collection.
-      * name in our case is `Whynesspower` and symbol is `CD`.
+      * name in our case is `whynesspower` and symbol is `CD`.
       * Constructor for Crypto Devs takes in the baseURI to set _baseTokenURI for the collection.
       * It also initializes an instance of whitelist interface.
       */
@@ -62,3 +62,58 @@ contract Whynesspower is ERC721Enumerable, Ownable {
     /**
       * @dev presaleMint allows a user to mint one NFT per transaction during the presale.
       */
+    function presaleMint() public payable onlyWhenNotPaused {
+        require(presaleStarted && block.timestamp < presaleEnded, "Presale is not running");
+        require(whitelist.whitelistedAddresses(msg.sender), "You are not whitelisted");
+        require(tokenIds < maxTokenIds, "Exceeded maximum Crypto Devs supply");
+        require(msg.value >= _price, "Ether sent is not correct");
+        tokenIds += 1;
+        //_safeMint is a safer version of the _mint function as it ensures that
+        // if the address being minted to is a contract, then it knows how to deal with ERC721 tokens
+        // If the address being minted to is not a contract, it works the same way as _mint
+        _safeMint(msg.sender, tokenIds);
+    }
+
+    /**
+    * @dev mint allows a user to mint 1 NFT per transaction after the presale has ended.
+    */
+    function mint() public payable onlyWhenNotPaused {
+        require(presaleStarted && block.timestamp >=  presaleEnded, "Presale has not ended yet");
+        require(tokenIds < maxTokenIds, "Exceed maximum Crypto Devs supply");
+        require(msg.value >= _price, "Ether sent is not correct");
+        tokenIds += 1;
+        _safeMint(msg.sender, tokenIds);
+    }
+
+    /**
+    * @dev _baseURI overrides the Openzeppelin's ERC721 implementation which by default
+    * returned an empty string for the baseURI
+    */
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    /**
+    * @dev setPaused makes the contract paused or unpaused
+      */
+    function setPaused(bool val) public onlyOwner {
+        _paused = val;
+    }
+
+    /**
+    * @dev withdraw sends all the ether in the contract
+    * to the owner of the contract
+      */
+    function withdraw() public onlyOwner  {
+        address _owner = owner();
+        uint256 amount = address(this).balance;
+        (bool sent, ) =  _owner.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+
+      // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {}
+}
