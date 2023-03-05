f23e46b82782eaddfd94c5458994d5b5925f28ec
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-05 21:17:15 +0530

    creating hooks

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -4,3 +4,19 @@ import React, { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
 import { abi, NFT_CONTRACT_ADDRESS } from "../constants";
 import styles from "../styles/Home.module.css";
+export default function Home() {
+    // walletConnected keep track of whether the user's wallet is connected or not
+    const [walletConnected, setWalletConnected] = useState(false);
+    // presaleStarted keeps track of whether the presale has started or not
+    const [presaleStarted, setPresaleStarted] = useState(false);
+    // presaleEnded keeps track of whether the presale ended
+    const [presaleEnded, setPresaleEnded] = useState(false);
+    // loading is set to true when we are waiting for a transaction to get mined
+    const [loading, setLoading] = useState(false);
+    // checks if the currently connected MetaMask wallet is the owner of the contract
+    const [isOwner, setIsOwner] = useState(false);
+    // tokenIdsMinted keeps track of the number of tokenIds that have been minted
+    const [tokenIdsMinted, setTokenIdsMinted] = useState("0");
+    // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
+    const web3ModalRef = useRef();
+  
