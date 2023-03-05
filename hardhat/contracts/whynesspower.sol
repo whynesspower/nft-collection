b2f16cd2f58769a4ccc7ab62f31a910400c1e124
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-05 18:58:19 +0530

    adding nft collection smart contract

---
+++ hardhat/contracts/whynesspower.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IWhitelist.sol";
+
+contract Whynesspower is ERC721Enumerable, Ownable {
+    /**
+      * @dev _baseTokenURI for computing {tokenURI}. If set, the resulting URI for each
+      * token will be the concatenation of the `baseURI` and the `tokenId`.
+      */
+    string _baseTokenURI;
+
+    //  _price is the price of one Crypto Dev NFT
+    uint256 public _price = 0.01 ether;
+
+    // _paused is used to pause the contract in case of an emergency
+    bool public _paused;
+
+    // max number of CryptoDevs
+    uint256 public maxTokenIds = 20;
+
+    // total number of tokenIds minted
+    uint256 public tokenIds;
+
+    // Whitelist contract instance
+    IWhitelist whitelist;
+
+    // boolean to keep track of whether presale started or not
+    bool public presaleStarted;
+
+    // timestamp for when presale would end
+    uint256 public presaleEnded;
+
+    modifier onlyWhenNotPaused {
+        require(!_paused, "Contract currently paused");
+        _;
+    }
+
+    /**
+      * @dev ERC721 constructor takes in a `name` and a `symbol` to the token collection.
+      * name in our case is `Whynesspower` and symbol is `CD`.
+      * Constructor for Crypto Devs takes in the baseURI to set _baseTokenURI for the collection.
+      * It also initializes an instance of whitelist interface.
+      */
+    constructor (string memory baseURI, address whitelistContract) ERC721("Whynesspower", "CD") {
+        _baseTokenURI = baseURI;
+        whitelist = IWhitelist(whitelistContract);
+    }
+
+    /**
+    * @dev startPresale starts a presale for the whitelisted addresses
+      */
+    function startPresale() public onlyOwner {
+        presaleStarted = true;
+        // Set presaleEnded time as current timestamp + 5 minutes
+        // Solidity has cool syntax for timestamps (seconds, minutes, hours, days, years)
+        presaleEnded = block.timestamp + 5 minutes;
+    }
+
+    /**
+      * @dev presaleMint allows a user to mint one NFT per transaction during the presale.
+      */
